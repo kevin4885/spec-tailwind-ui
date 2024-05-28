133c189271ea80a507301be78021fb5f564f2215
kevinj-ts <153937527+kevinj-ts@users.noreply.github.com>
2024-05-28 18:05:01 -0500

    Update TailwindCSS and TextArea component
    This commit refactors the TailwindCSS config to use 'spec-tailwind-ui' instead of 'tailwind-ui'. Additionally, the TextArea component has been improved to adjust its height based on its content. Also, updated the version number in package.json.

--- README.md
+++ README.md
@@ -2,26 +2,31 @@
 
 ```javascript
 /** @type {import('tailwindcss').Config} */
-import {tailwindColors} from 'tailwind-ui'
+import {tailwindColors} from 'spec-tailwind-ui'
 export default {
-  darkMode: 'class', <--Added
+  darkMode: 'class', // Added
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/tailwind-ui/**/*.{js,jsx,ts,tsx}" <--Added
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+      "./src/**/*Class.{js,jsx,ts,tsx}", // Added
+      "./src/**/*Classes.{js,jsx,ts,tsx}", // Added
+      "./node_modules/spec-tailwind-ui/**/*.{js,jsx,ts,tsx}", // Added
+      "./node_modules/spec-tailwind-ui/**/*Class.{js,jsx,ts,tsx}", // Added
+      "./node_modules/spec-tailwind-ui/**/*Classes.{js,jsx,ts,tsx}" // Added
   ],
+  
   theme: {
-    colors: tailwindColors, <--Added
+    colors: tailwindColors, // Added
     extend: {},
   },
   plugins: [],
 }
 ```
-
+~~~~
 ## Example
 
 ```javascript
-import { setPageTheme } from 'tailwind-ui';
+import { setPageTheme } from 'spec-tailwind-ui';
 import { useEffect } from 'react';
 
 function App() {
@@ -31,6 +36,7 @@ function App() {
 
 export default App
 ```
+
 
 
 
--- src/components/controls/TextArea.jsx
+++ src/components/controls/TextArea.jsx
@@ -1,34 +1,44 @@
-import { forwardRef, useCallback, useRef } from 'react';
+import {forwardRef, useCallback, useEffect, useRef} from 'react';
 import BaseInput from './BaseInput';
 
 const TextArea = forwardRef(function (
-  { className, autoGrow, onChange, value = '', label, description, messages,  ...rest },
-  ref,
+    {className, autoGrow, onChange, value = '', label, description, messages, ...rest},
+    ref,
 ) {
-  const innerRef = useRef(null);
+    const innerRef = useRef(null);
 
-  const setRefs = useCallback(
-    (el) => {
-      ref && (ref.current = el);
-      innerRef.current = el;
-    },
-    [ref, innerRef],
-  );
+    const setRefs = useCallback(
+        (el) => {
+            ref && (ref.current = el);
+            innerRef.current = el;
+        },
+        [ref, innerRef],
+    );
 
-  const handleChange = (e) => {
-    if (autoGrow && innerRef?.current) {
-      innerRef.current.style.height = '0px';
-      const scrollHeight = innerRef.current.scrollHeight;
-      innerRef.current.style.height = scrollHeight + 'px';
-    }
-    onChange && onChange({ value: e.target?.value, messages: [], event: e, hasErrors: false });
-  };
+    const adjustHeight = useCallback(() => {
+        if (autoGrow && innerRef?.current) {
+            innerRef.current.style.height = '0px';
+            const scrollHeight = innerRef.current.scrollHeight + 10;
+            innerRef.current.style.height = scrollHeight + 'px';
+        }
+    }, [autoGrow]);
 
-  return (
-    <BaseInput className={className} label={label} description={description} messages={messages} {...rest}>
-      <textarea ref={setRefs} value={value} onChange={handleChange} {...rest} />
-    </BaseInput>
-  );
+    useEffect(() => {
+        adjustHeight();
+    }, [value, adjustHeight]);
+
+    const handleChange = (e) => {
+        adjustHeight();
+        onChange && onChange(e);
+    };
+
+
+    return (
+        <BaseInput className={className} label={label} description={description} messages={messages} value={value}
+                   onChange={handleChange} {...rest}>
+            <textarea ref={setRefs}  {...rest} />
+        </BaseInput>
+    );
 });
 
 export default TextArea;
